1fa19539052467d19e2ea02c17babf01ce795197
leogoncalvestech <leonardo.10goncalves@hotmail.com>
2025-04-10 22:19:22 -0300

    feat :dbt run sql

---
+++ dbsales/models/staging/stg_commodities.sql
@@ -0,0 +1,21 @@
+-- import
+with source as (
+    select  
+        "Date",
+        "Close",
+        "simbolo"
+    from {{ source ("dbsales" , "commodities")}}
+
+),
+-- renamed
+renamed as (
+
+    select 
+      cast( "Date" as date ) as data ,
+      'Close' as valor_fechamento,
+      simbolo
+    from 
+        source
+)
+
+select * from renamed
