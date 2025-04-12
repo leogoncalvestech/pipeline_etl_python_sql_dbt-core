7b6012150905ee3afca0884858c1511ba835d18a
leogoncalvestech <leonardo.10goncalves@hotmail.com>
2025-04-12 16:48:09 -0300

    feat:adicionar datamart e staging

---
+++ dbsales/models/staging/stg_movimentacao_commodities.sql
@@ -0,0 +1,28 @@
+
+
+
+with source as (
+    select 
+        date,
+        symbol,
+        action,
+        quantity
+    from
+       {{source('dbsales','movimentacao_commodities')}}
+
+
+
+),
+
+renamed as (
+    select 
+        cast(date as date)as data,
+        symbol as simbolo,
+        action as acao,
+        quantity as quantidade
+    from source
+
+
+)
+
+select * from renamed
